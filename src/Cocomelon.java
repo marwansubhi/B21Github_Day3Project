2b61c013b0244cf6f504f7169053aba8c233d6cf
Marwan Subhi <marwan.subhi4@gmail.com>
2020-11-12 11:24:15 -0500

    added another note

--- src/Cocomelon.java
+++ src/Cocomelon.java
@@ -2,5 +2,6 @@ public class Cocomelon {
     public static void main(String[] args) {
         System.out.println("Hello everyone");
         // good to add notes
+        // we are progressing
     }
 }
